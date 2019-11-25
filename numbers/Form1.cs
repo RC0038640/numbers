83a1c487285a0ac65616691eaff315c0908b8386
James Gittens <rc0038640@student.riversidecollege.ac.uk>
2019-11-25 15:20:13 +0000

    pi

--- numbers/Form1.cs
+++ numbers/Form1.cs
@@ -15,6 +15,24 @@ namespace numbers
         public Form1()
         {
             InitializeComponent();
+            double d1 = 5e6;
+            richTextBox1.AppendText(d1.ToString() + "\n");
+            richTextBox1.AppendText(d1.ToString("N") + "\n");
+            richTextBox1.AppendText(d1.ToString("N3") + "\n");
+            richTextBox1.AppendText(d1.ToString("N0") + "\n");
+            richTextBox1.AppendText(d1.ToString("0") + "\n");
+            richTextBox1.AppendText(d1.ToString("0.0") + "\n");
+            richTextBox1.AppendText(d1.ToString("0.0E0") + "\n");
+            richTextBox1.AppendText(d1.ToString("0.00") + "\n");
+            double pi = 3.14159265;
+            richTextBox1.AppendText(pi.ToString("N3") + "\n");
+            richTextBox1.AppendText(pi.ToString("N5") + "\n");
+            richTextBox1.AppendText(pi.ToString("N15") + "\n");
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
